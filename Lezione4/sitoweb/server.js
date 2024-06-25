5983fc1e593f3b233326b834b761651b058cfd3f
Nicolas7777777 <sairwater@gmail.com>
2024-06-25 17:00:10 +0200

    3 commit

--- Lezione4/sitoweb/server.js
+++ Lezione4/sitoweb/server.js
@@ -8,10 +8,11 @@ var iPortaTcp = 4201;
 var sIpAddress = "127.0.0.1"
 
 app.listen(iPortaTcp,sIpAddress, () => console.log('API is running on http://' + sIpAddress + ':' + iPortaTcp));
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+//const bodyParser = require('body-parser');
+//app.use(bodyParser.urlencoded({ extended: true }));
+
 //PRIMA ROUTE
 app.get('', (req, res) => {
-//console.log("Mi hai chiesto la pagina iniziale");
-//res.sendFile("index.html", { root: './htdocs' });
+console.log("Mi hai chiesto la pagina iniziale");
+res.sendFile("index.html", { root: './htdocs' });
 });
